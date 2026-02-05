684396a5077e7ee3bb54c9bc89335adae7478754
Teufelauto <110637430+Teufelauto@users.noreply.github.com>
2026-02-05 02:01:48 -0500

    Load assets from zip
    Takes a lot longer to run, but no need to un-zip every new release.

--- Weapons/WeaponShared.gd
+++ Weapons/WeaponShared.gd
@@ -3,15 +3,71 @@ class_name WeaponShared
 # A common place to put shared data
 
 
+# TODO:  Add Template weapons. They add data to some weapons.
 
-const melee_weapon_shared_list: Array = [WeaponShared.battleaxe, 
-										WeaponShared.daggers, 
-										WeaponShared.mace, 
-										WeaponShared.sword,
-										]
+var melee_weapon_shared_list: Array = [
+	WeaponShared.battleaxe, 
+	WeaponShared.daggers, 
+	WeaponShared.mace, 
+	WeaponShared.sword,
+	]
+var total_number_of_weapons: int = \
+	WeaponShared.battleaxe.weapon_descriptor.size() + \
+	WeaponShared.daggers.weapon_descriptor.size() + \
+	WeaponShared.mace.weapon_descriptor.size() + \
+	WeaponShared.sword.weapon_descriptor.size()
+var weapon_table_columns: Array[String] = [
+	"Item_Count",
+	"ID",
+	"Weapon_Family",
+	"Descriptor",
+	"Pregenerated_Icon",
+	"Item_Level",
+	"Quality",
+	"Max_Durability",
+	"Durability_Loss_On_Hit",
+	"primary_attack_1_damage",
+	"primary_attack_2_damage",
+	"primary_attack_3_damage",
+	"primary_attack_4_damage",
+	"charged_attack_1_damage",
+	"charged_attack_2_damage",
+	"charged_attack_3_damage",
+	"signature_attack_1_damage",
+	"signature_attack_2_damage",
+	]
 
-class battleaxe:
-	const weapon: String = "Battleaxe"
+# "filename_in_zip": "Template_Weapon_Sword"
+var template_weapon_battleaxe: Array =[
+	"Quality",
+	"ItemLevel",
+	"MaxDurability",
+	"DurabilityLossOnHit",
+	]
+var template_weapon_daggers: Array =[
+	"Quality",
+	"ItemLevel",
+	"MaxDurability",
+	"DurabilityLossOnHit",
+	]
+var template_weapon_mace: Array =[
+	"Quality",
+	"ItemLevel",
+	"MaxDurability",
+	"DurabilityLossOnHit",
+	]
+var template_weapon_sword: Array =[
+	"Quality",
+	"ItemLevel",
+	"MaxDurability",
+	"DurabilityLossOnHit",
+	]
+
+
+
+class battleaxe: # Filename in zip is  weapon + "_" + weapon_descriptor
+	const weapon_family: String = "Battleaxe" # Must be Capatalized for file path to work inside zip files.
+	const parent: String ="Template_Weapon_Battleaxe" # Must be Capatalized for file path to work inside zip files.
 	const primary_attack_1_name: String   = "Swing_Down_Left"
 	const primary_attack_2_name: String   = "Swing_Down_Right"
 	const primary_attack_3_name: String   = "Swing_Down"
@@ -21,25 +77,28 @@ class battleaxe:
 	const charged_attack_3_name: String   = ""
 	const signature_attack_1_name: String = "Signature_Whirlwind"
 	const signature_attack_2_name: String = ""
-	const weapon_descriptor: Array[String] = ["Adamantite",
-									"Cobalt",
-									"Copper",
-									"Crude",
-									"Doomed",
-									"Iron",
-									"Mithril",
-									"Onyxium",
-									"Scarab",
-									"Scythe_Void",
-									"Steel_Rusty",
-									"Stone_Trork",
-									"Thorium",
-									"Tribal",
-									"Wood_Fence",
-									]
+	# Weapon descriptor must match capitalization in filename in zip.
+	const weapon_descriptor: Array[String] = [
+		"Adamantite",
+		"Cobalt",
+		"Copper",
+		"Crude",
+		"Doomed",
+		"Iron",
+		"Mithril",
+		"Onyxium",
+		"Scarab",
+		"Scythe_Void",
+		"Steel_Rusty",
+		"Stone_Trork",
+		"Thorium",
+		"Tribal",
+		"Wood_Fence",
+		]
 
-class daggers:
-	const weapon: String = "Daggers"
+class daggers: # Filename in zip is:  weapon + "_" + weapon_descriptor
+	const weapon_family: String = "Daggers" # Must be Capatalized for file path to work inside zip files.
+	const parent: String ="Template_Weapon_Daggers" # Must be Capatalized for file path to work inside zip files.
 	const primary_attack_1_name: String   = "Swing_Left"
 	const primary_attack_2_name: String   = "Swing_Right"
 	const primary_attack_3_name: String   = "Stab_Left"
@@ -49,26 +108,29 @@ class daggers:
 	const charged_attack_3_name: String   = ""
 	const signature_attack_1_name: String = "Razorstrike_Slash"
 	const signature_attack_2_name: String = "Razorstrike_Sweep"
-	const weapon_descriptor: Array[String] = ["Adamantite",
-									"Adamantite_Saurian",
-									"Bone",
-									"Bronze",
-									"Bronze_Ancient",
-									"Claw_Bone",
-									"Cobalt",
-									"Copper",
-									"Crude",
-									"Doomed",
-									"Fang_Doomed",
-									"Iron",
-									"Mithril",
-									"Onyxium",
-									"Stone_Trork",
-									"Thorium",
-									]
+	# Weapon descriptor must match capitalization in filename in zip.
+	const weapon_descriptor: Array[String] = [
+		"Adamantite",
+		"Adamantite_Saurian",
+		"Bone",
+		"Bronze",
+		"Bronze_Ancient",
+		"Claw_Bone",
+		"Cobalt",
+		"Copper",
+		"Crude",
+		"Doomed",
+		"Fang_Doomed",
+		"Iron",
+		"Mithril",
+		"Onyxium",
+		"Stone_Trork",
+		"Thorium",
+		]
 
-class mace:
-	const weapon: String = "Mace"
+class mace: # Filename in zip is:  weapon + "_" + weapon_descriptor
+	const weapon_family: String = "Mace" # Must be Capatalized for file path to work inside zip files.
+	const parent: String ="Template_Weapon_Mace" # Must be Capatalized for file path to work inside zip files.
 	const primary_attack_1_name: String   = "Swing_Left"
 	const primary_attack_2_name: String   = "Swing_Right"
 	const primary_attack_3_name: String   = "Swing_Up_Left"
@@ -78,22 +140,25 @@ class mace:
 	const charged_attack_3_name: String   = "Swing_Up_Left_Charged"
 	const signature_attack_1_name: String = "Groundslam"
 	const signature_attack_2_name: String = ""
-	const weapon_descriptor: Array[String] = ["Adamantite",
-									"Cobalt",
-									"Copper",
-									"Crude",
-									"Iron",
-									"Mithril",
-									"Onyxium",
-									"Prisma",
-									"Scrap",
-									"Scrap_NPC",
-									"Stone_Trork",
-									"Thorium",
-									]
+	# Weapon descriptor must match capitalization in filename in zip.
+	const weapon_descriptor: Array[String] = [
+		"Adamantite",
+		"Cobalt",
+		"Copper",
+		"Crude",
+		"Iron",
+		"Mithril",
+		"Onyxium",
+		"Prisma",
+		"Scrap",
+		"Scrap_NPC",
+		"Stone_Trork",
+		"Thorium",
+		]
 	
-class sword:
-	const weapon: String = "Sword"
+class sword: # Filename in zip is:  weapon + "_" + weapon_descriptor
+	const weapon_family: String = "Sword" # Must be Capatalized for file path to work inside zip files.
+	const parent: String ="Template_Weapon_Sword" # Must be Capatalized for file path to work inside zip files.
 	const primary_attack_1_name: String   = "Swing_Left"
 	const primary_attack_2_name: String   = "Swing_Right"
 	const primary_attack_3_name: String   = "Swing_Down"
@@ -103,30 +168,32 @@ class sword:
 	const charged_attack_3_name: String   = ""
 	const signature_attack_1_name: String = "Vortexstrike_Spin"
 	const signature_attack_2_name: String = "Vortexstrike_Stab"
-	const weapon_descriptor: Array[String] = ["Adamantite",
-									"Bone",
-									"Bronze",
-									"Bronze_Ancient",
-									"Cobalt",
-									"Copper",
-									"Crude",
-									"Cutlass",
-									"Doomed",
-									"Frost",
-									"Iron",
-									"Mithril",
-									"Nexus",
-									"Onyxium",
-									"Runic",
-									"Scrap",
-									"Silversteel",
-									"Steel",
-									"Steel_Incandescent",
-									"Steel_Rusty",
-									"Stone_Trork",
-									"Thorium",
-									"Wood",
-									]
+	# Weapon descriptor must match capitalization in filename in zip.
+	const weapon_descriptor: Array[String] = [
+		"Adamantite",
+		"Bone",
+		"Bronze",
+		"Bronze_Ancient",
+		"Cobalt",
+		"Copper",
+		"Crude",
+		"Cutlass",
+		"Doomed",
+		"Frost",
+		"Iron",
+		"Mithril",
+		"Nexus",
+		"Onyxium",
+		"Runic",
+		"Scrap",
+		"Silversteel",
+		"Steel",
+		"Steel_Incandescent",
+		"Steel_Rusty",
+		"Stone_Trork",
+		"Thorium",
+		"Wood",
+		]
 	
 	
 	
--- main.gd
+++ main.gd
@@ -1,57 +1,82 @@
 extends Control
 
+# TODO get weaponshared data onto user folder as a json or something so it can be edited without recompile.
+const MyWeaponShared = preload("res://Weapons/WeaponShared.gd") 
+var my_weapons: MyWeaponShared
 
-var item_asset_path:String ="user://item/items/weapon/" # TODO Need to make a zip reader instead!
-var save_path: String = "user://weapon_table.csv"
+
+var asset_zip_path: String = "user://Assets.zip"
+var csv_save_path: String = "user://weapon_table.csv"
 var zip_reader := ZIPReader.new()
 
-
-var current_table_row: int = 0  # for incrementing each weapon for entry into matrix
-#Determine how many rows are in the weapon_table by counting each weapon's descriptors
-var total_number_of_weapons:int = WeaponShared.battleaxe.weapon_descriptor.size() + \
-							WeaponShared.daggers.weapon_descriptor.size() + \
-							WeaponShared.mace.weapon_descriptor.size() + \
-							WeaponShared.sword.weapon_descriptor.size()
-var weapon_table_width: int = 18 # columns in array. See initialize_weapon_table() for list
-var weapon_table_height: int = total_number_of_weapons + 1 # Add 1 for the column headers
-var weapon_table: Array[Array] = []
-
+# Weapon Table construction
+# Determine how many rows are in the weapon_table by counting each weapon's descriptors
+var total_number_of_weapons:int
+var weapon_table_height: int 
+var weapon_table_columns: Array
+var weapon_table_width: int
+var weapon_table: Array[Array] = [] # Initialize the empty table.
+var current_table_row: int = 0  # initialize for incrementing each weapon for entry into matrix
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	check_if_first_load() # Creates settings file in user folder.
 	# TODO:  Check if Headless from App_Settings.json, and deal with that.
 	
-	# TODO: Check it Asset.zip exists before trying to load
-	open_assets_zip()
-	
+	my_weapons = MyWeaponShared.new() # for calling WeaponShared
+	open_assets_zip() # Open ZIP reader
 	initialize_weapon_table()
+	
+	scrape_template_weapon_items()
+	
 	step_through_weapons()
+	
+	zip_reader.close() #Close ZIP reader
+	
 	print_weapon_table_to_console()
-	save_array_as_csv(weapon_table, save_path)
+	save_array_as_csv(weapon_table, csv_save_path)
 
-
+# TODO Gett scraped data onto the table, either before or during populating.
+func scrape_template_weapon_items() -> void:
+	
+	pass
 
 func open_assets_zip()->void:
-	var error = zip_reader.open("user://Assets.zip")
+	var error = zip_reader.open(asset_zip_path)
 	if error != OK:
 		print("Failed to open ZIP file: ", error)
 		return
 
-
 # Sets up app the first time it is loaded
 func check_if_first_load() -> void:
 	
-	# Need to copy the app setttings into the user folder, 
+	# Need to copy the App Setttings into the user folder, 
 	# so they can be edited by the user by headless method.
 	var setttings_file: String = "app_settings.json"
 	var not_first_time: bool = check_user_file_exists(setttings_file)
-	if not_first_time: # Meaning, if the file exists in the user folder, skip all this.
-		return
+	var full_source: String
+	var full_destination: String
+	if not_first_time: # Meaning, if the file exists in the user folder, skip
+		print("User folder contains app_settings")
 	else:
-		var full_source: String = "res://" + setttings_file
-		var full_destination: String = "user://" + setttings_file
-		
+		full_source = "res://" + setttings_file
+		full_destination = "user://" + setttings_file
+		copy_file_from_res_to_user(full_source, full_destination) # copy app_settings to user
+
+	# TODO Weapon Setup resource
+	#var weapons_file: String = "weapon_setup.json"
+	#not_first_time = check_user_file_exists(weapons_file)
+	#if not_first_time: # Meaning, if the file exists in the user folder, skip all this.
+		#print("User folder contains WeaponShared")
+	#else:
+		#full_source = "res://weapons/" + weapons_file
+		#full_destination = "user://" + weapons_file
+		#copy_file_from_res_to_user(full_source, full_destination) # copy WeaponShared to user
+	
+	
+
+
+func copy_file_from_res_to_user (full_source: String, full_destination: String) -> void:
 	# Use DirAccess.copy_absolute()
 	# It copies a file from an absolute source path to an absolute destination path.
 	# Note: The destination path should include the new file name.
@@ -61,7 +86,7 @@ func check_if_first_load() -> void:
 			print("File copied successfully to: ", full_destination)
 		else:
 			print("Error copying file: ", error_string(error))
-
+	
 
 func check_user_file_exists(file_name: String) -> bool:
 	# Construct the full path using the 'user://' shorthand
@@ -95,6 +120,12 @@ func save_array_as_csv(data: Array, path: String) -> void:
 # This is the 2d array, matrix, or table, where the info scraped from the JSONs gets put.
 # The table can be exported as CSV or used internally. 
 func initialize_weapon_table() -> void:
+	# Define the table size from tthe weapon shared file
+	total_number_of_weapons = my_weapons.total_number_of_weapons
+	weapon_table_height = total_number_of_weapons + 1 # Add 1 for the column headers
+	weapon_table_columns = my_weapons.weapon_table_columns
+	weapon_table_width = weapon_table_columns.size() # columns in array
+	
 	# Create the outer array
 	for row in range(weapon_table_height):
 		# Append inner arrays to form the 2D structure
@@ -103,26 +134,9 @@ func initialize_weapon_table() -> void:
 			# Initialize each cell with a default value (e.g., 0 for an empty cell)
 			weapon_table[row].append(null)
 	# You can then set specific values. weapon_table[Row][Column]
-	# \/\/\/\/\/\/\ Be sure weapon_table_width equals columns below! \/\/\/\/\/\/\/
-	# These are the Column Headers for the table
-	weapon_table[0][0] = "Item_Count"
-	weapon_table[0][1] = "ID"
-	weapon_table[0][2] = "Weapon"
-	weapon_table[0][3] = "Descriptor"
-	weapon_table[0][4] = "Pregenerated_Icon"
-	weapon_table[0][5] = "Item_Level"
-	weapon_table[0][6] = "Quality"
-	weapon_table[0][7] = "Max_Durability"
-	weapon_table[0][8] = "Durability_Loss_On_Hit"
-	weapon_table[0][9] = "primary_attack_1_damage"
-	weapon_table[0][10] = "primary_attack_2_damage"
-	weapon_table[0][11] = "primary_attack_3_damage"
-	weapon_table[0][12] = "primary_attack_4_damage"
-	weapon_table[0][13] = "charged_attack_1_damage"
-	weapon_table[0][14] = "charged_attack_2_damage"
-	weapon_table[0][15] = "charged_attack_3_damage"
-	weapon_table[0][16] = "signature_attack_1_damage"
-	weapon_table[0][17] = "signature_attack_2_damage"
+	# Populate the Column Headers for the table.
+	for column in range(weapon_table_width):
+		weapon_table[0][column] = weapon_table_columns[column]
 
 
 func print_weapon_table_to_console() -> void:
@@ -135,40 +149,43 @@ func print_weapon_table_to_console() -> void:
 func step_through_weapons() -> void:
 	current_table_row = 0 #start with 0 and increment each j
 	
-	#select weapon class- battleaxe, dagger etc
-	for i in WeaponShared.melee_weapon_shared_list:
-		var CurrentWeapon: Object = i
+	#select weapon family- battleaxe, dagger etc
+	for i in my_weapons.melee_weapon_shared_list:
+		var CurrentWeapon: Object = i # dagger or sword subclass etc
 	
 		for j in CurrentWeapon.weapon_descriptor:
 			current_table_row +=1
 			#select weapon descriptor- crude, adamantite, etc
-			var current_weapon_descriptor:String = j
-			scrape_weapon(CurrentWeapon, current_weapon_descriptor)
+			var current_weapon_descriptor:String = j # "Crude" or "iron" etc
+			scrape_weapon_item_data(CurrentWeapon, current_weapon_descriptor)
 	
 # TODO Need to use TEMPLATE json for info not in individual files when exists.
-func scrape_weapon(CurrentWeapon: Object,current_weapon_descriptor: String) -> void:
+# CurrentWeapon is tthe weapon class (sword etc) and current_weapon_descriptor is "crude" or "iron" etc
+func scrape_weapon_item_data(CurrentWeapon: Object,current_weapon_descriptor: String) -> void:
 	#counting index for putting each item in its own row on table
 	weapon_table[current_table_row][0] = current_table_row
 	print(current_table_row)
 	
-	# for creating filepath of the item
-	var current_weapon_as_string: String = CurrentWeapon.weapon
-	var weapon_id: String = current_weapon_as_string + "_" + current_weapon_descriptor
+	var current_weapon_family_as_string: String = CurrentWeapon.weapon_family # "Battleaxe" or "Sword" etc
+	var weapon_id: String = current_weapon_family_as_string + "_" + current_weapon_descriptor  # "Sword_Crude" etc
 	print(weapon_id)
-	weapon_table[current_table_row][1] = weapon_id
-	weapon_table[current_table_row][2] = current_weapon_as_string
+	weapon_table[current_table_row][1] = weapon_id # Unique name for each item
+	weapon_table[current_table_row][2] = current_weapon_family_as_string
 	weapon_table[current_table_row][3] = current_weapon_descriptor
 	
-	var file_path: String = weapon_filepath(current_weapon_as_string, weapon_id) #need the file path and name of the current weapon
+	#need the file path and name of the current weapon
+	var _items_path: String = create_weapon_items_filepath(current_weapon_family_as_string, weapon_id) 
 	
 	# ============= make dictionary from the json ==========================
-	var item_weapon_as_dict: Dictionary = parse_weapon_info(file_path)
+	var item_weapon_as_dict: Dictionary = parse_weapon_item_info(_items_path)
 	#======================================================================
 	
 	# Retrieve top-level dictionary stuff
-	var item_icon: String = item_weapon_as_dict.get("Icon", "Unknown")
-	print("Pregenerated Icon: ", item_icon)
-	weapon_table[current_table_row][4] = item_icon
+	# Fill unknowns with template data.
+	
+	var _item_icon: String = item_weapon_as_dict.get("Icon", "Unknown")
+	print("Pregenerated Icon: ", _item_icon)
+	weapon_table[current_table_row][4] = _item_icon
 	var item_level: int = item_weapon_as_dict.get("ItemLevel", 0) #Can use for sorting
 	print("Item Level: ", item_level)
 	weapon_table[current_table_row][5] = item_level
@@ -242,19 +259,28 @@ func scrape_weapon(CurrentWeapon: Object,current_weapon_descriptor: String) -> v
 	print("")# Seperates each iteration
 
 
-# Create the file path and name, so json can be loaded.
-func weapon_filepath(weapon_type: String, weapon_id: String) -> String:
-	var _pathstring: String = item_asset_path + weapon_type + "/weapon_" + weapon_id + ".json" 
+# Create the file path and name, so json can be loaded inside assets.zip
+# Holey Canolli, it's case-sensative.
+func create_weapon_items_filepath(weapon_type: String, weapon_id: String) -> String:
+	var _pathstring: String = "Server/Item/Items/Weapon/" + weapon_type + "/Weapon_" + weapon_id + ".json" 
 	print(_pathstring)
 	return _pathstring
 	
 	
-#Parse weapon item/items json and turn it into a Dictionary
-func parse_weapon_info(file_path: String) -> Dictionary:
-	var item_weapon_string: String = FileAccess.get_file_as_string(file_path)
-	var item_weapon_as_dict: Dictionary = JSON.parse_string(item_weapon_string)
-	return item_weapon_as_dict
-
+#Parse weapon server/item/items damage info json and turn it into a Dictionary 
+func parse_weapon_item_info(file_path_inside_zip: String) -> Dictionary:
+	# Read json inside zip
+	var file_buffer: PackedByteArray = zip_reader.read_file(file_path_inside_zip)
+	
+	if file_buffer.is_empty():
+		print("Failed to read file or file is empty")
+		return {null:null}
+	else:
+		print("Successfully read file: ", file_path_inside_zip)
+		# Convert Byte Array into String. utf8 for safety
+		var _item_weapon_info_string: String = file_buffer.get_string_from_utf8()     # FileAccess.get_file_as_string(file_path)
+		var _item_weapon_info_as_dict: Dictionary = JSON.parse_string(_item_weapon_info_string)
+		return _item_weapon_info_as_dict
 
 
 #Array inside json needs special treatment. all the elses are for if key doesn't exist in json
